1ac864e0b26c989bef468d151e259c37388230b3
Fatah Nur Alam Majid <fatahnuram@gmail.com>
2024-01-18 17:14:42 +0700

    feat: question 3

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -9,7 +10,8 @@ import (
 func main() {
 	fmt.Println("Hello, world!")
 
-	// contoh Println vs Sprintln
+	/* contoh Println vs Sprintln */
+
 	// Println
 	fmt.Println("Example of Println function.")
 
@@ -18,4 +20,15 @@ func main() {
 	// Sprintln (dan di-print)
 	s := fmt.Sprintln("Another example of Sprintln function.")
 	io.WriteString(os.Stdout, s)
+
+	/* contoh fmt.Errorf vs errors.New */
+
+	// fmt.Errorf
+	errcontent := "contoh dynamic error msg"
+	err := fmt.Errorf("example Errorf msg: %s", errcontent)
+	fmt.Println(err)
+
+	// errors.New
+	err2 := errors.New("example errors.New for static error msg")
+	fmt.Println(err2)
 }
