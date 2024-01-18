f8932378bd45fcafa4659dad5ffeeb4033ab98c3
Fatah Nur Alam Majid <fatahnuram@gmail.com>
2024-01-18 16:13:41 +0700

    feat: question 1-2

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+
+	// contoh Println vs Sprintln
+	// Println
+	fmt.Println("Example of Println function.")
+
+	// Sprintln (tapi tidak di-print)
+	fmt.Sprintln("Example of Sprintln function.")
+	// Sprintln (dan di-print)
+	s := fmt.Sprintln("Another example of Sprintln function.")
+	io.WriteString(os.Stdout, s)
 }
