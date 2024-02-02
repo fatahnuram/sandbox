b6997114d955c210efe5f6b2511d4fbfabd03067
Fatah Nur Alam Majid <fatahnuram@gmail.com>
2024-02-02 22:23:22 +0700

    feat: tutorial sql

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # sandbox
+
+HSI Sandbox #3 Golang track.
+
+## How to
+
+Run dev database using `mysql:8.0` image:
+
+```bash
+docker run -d --name mysqldb -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password mysql:8.0
+```
+
+Access the database container:
+
+```bash
+docker exec -it mysqldb sh
+# or
+docker exec -it mysqldb mysql -u root -ppassword
+```
+
+Build the app:
+
+```bash
+go build .
+```
+
+Run the app:
+
+```bash
+export DBUSER=root
+export DBPASS=password
+./sandbox
+```
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
 func main() {
 	// fmt.Println("Hello, world!")
 
@@ -26,7 +37,50 @@ func main() {
 	// err2 := errors.New("example errors.New for static error msg")
 	// fmt.Println(err2)
 
-	/* Level 2 */
-	generateNIK("akhwat", 2025, 2, 3)
-	generateNIKLanjutan("ARN201-00035", 3)
+	// /* Level 2 */
+	// generateNIK("akhwat", 2025, 2, 3)
+	// generateNIKLanjutan("ARN201-00035", 3)
+
+	/* Tutorial SQL */
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "recordings",
+	}
+
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+
+	alb, err := albumById(3)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", alb)
+
+	albId, err := addAlbum(Album{
+		Title:  "The Modern Sound of Betty Carter",
+		Artist: "Betty Carter",
+		Price:  49.99,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ID of added album: %v\n", albId)
 }
